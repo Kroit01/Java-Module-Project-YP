65d5b646f51f0333b50ef5fe948e8395bf0ee00b
AleksandrPanaetov <Alex.panaetov.96@gmail.com>
2026-02-12 12:10:54 +0400

    Проектная работа 1. скорректированная

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -5,7 +5,7 @@ public class Car {
     String name;
     int speed;
 
-public Car (String carName, int carSpeed){ //чтобы мог вызывать в других классах
+public Car (String carName, int carSpeed){ //чтобы мог вызывать в других классах22
     this.name = carName;
     this.speed = carSpeed;
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,21 +3,34 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Car car1 = new Car(null, 0); // создаем значения из класса Car
         Car car2 = new Car(null, 0);
         Car car3 = new Car(null, 0);
         Race race24 = new Race(); // чтобы можно было делать ссылки на класс Race
 
+
         System.out.println("Введите название машины номер 1");
-        car1.name = scanner.next();
+            car1.name = scanner.next();
 
         System.out.println("Введите скорость машины номер 1");
-        car1.speed = scanner.nextInt();
-        while (car1.speed < 0 || car1.speed > 250) {
-            System.out.println("Некорректная скорость. Попробуйте снова");
-            car1.speed = scanner.nextInt();
-        }
+
+        while (true){
+            if (scanner.hasNextInt()){
+                int tempSpeed = scanner.nextInt();
+                if (tempSpeed >= 0 && tempSpeed<=250){
+                    car1.speed = tempSpeed;
+                    break;
+                } else {
+                    System.out.println("Некорректная скорость. Попробуйте снова");
+                }
+        } else {
+                String trash = scanner.next();
+                System.out.println("Некорректная скорость. Попробуйте снова");
+            }
+}
+
+
 
         race24.calculateDistance(car1); //Считаем дистанцию и смотрим, лидер или нет
 
@@ -25,10 +38,21 @@ public class Main {
         car2.name = scanner.next();
 
         System.out.println("Введите скорость машины номер 2");
-        car2.speed = scanner.nextInt();
-        while (car2.speed < 0 || car1.speed > 250) {
-            System.out.println("Некорректная скорость. Попробуйте снова");
-            car2.speed = scanner.nextInt();
+        while (true){
+            if (scanner.hasNextInt()){
+                int tempSpeed = scanner.nextInt();
+                if (tempSpeed >= 0 && tempSpeed<=250){
+                    car2.speed = tempSpeed;
+                    break;
+                } else {
+                    System.out.println("Некорректная скорость. Попробуйте снова");
+                }
+            } else {
+                String trash = scanner.next();
+                System.out.println("Некорректная скорость. Попробуйте снова");
+            }
+
+
         }
         race24.calculateDistance(car2);//Считаем дистанцию и смотрим, лидер или нет
 
@@ -36,17 +60,24 @@ public class Main {
         car3.name = scanner.next();
 
         System.out.println("Введите скорость машины номер 3");
-        car3.speed = scanner.nextInt();
-        while (car3.speed < 0 || car1.speed > 250) {
-            System.out.println("Некорректная скорость. Попробуйте снова");
-            car3.speed = scanner.nextInt();
+        while (true){
+            if (scanner.hasNextInt()){
+                int tempSpeed = scanner.nextInt();
+                if (tempSpeed >= 0 && tempSpeed<=250){
+                    car3.speed = tempSpeed;
+                    break;
+                } else {
+                    System.out.println("Некорректная скорость. Попробуйте снова");
+                }
+            } else {
+                String trash = scanner.next();
+                System.out.println("Некорректная скорость. Попробуйте снова");
+            }
         }
         race24.calculateDistance(car3);//Считаем дистанцию и смотрим, лидер или нет
 
         race24.printWinner(); // объявляем лидера
     }
-
-
 
 
 }
