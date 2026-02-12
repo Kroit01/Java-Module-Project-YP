ce83e66fc39c45650f9a7b9b744f346ef49ba239
AleksandrPanaetov <Alex.panaetov.96@gmail.com>
2026-02-12 14:23:10 +0400

    Проектная работа 1. скорректированная. Добавил работу с некорректными данными. добавил возможность побеждать более 1 участнику. Убрал возможность повторения машин. Если значение введено несколькими цифрами - теперь ошибка, что так нельзя

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,17 +1,26 @@
+import java.util.ArrayList;
+
 public class Race {
 
-    String winnerName = ""; // чтобы записывать лидера
-    int winnerDistance = 0; // чтобы записывать дистанцию лидера и сравнивать
+    ArrayList<String> winners = new ArrayList<>();
+    int maxDistance = 0; // чтобы записывать дистанцию лидера и сравнивать
 
     public void calculateDistance (Car car){
        int distance = car.speed * 24; //считаем дистанцию
-       if (distance > winnerDistance) {
-           winnerDistance = distance; //
-           winnerName = car.name; // выбираем лидера
+       if (distance > maxDistance) {
+           maxDistance = distance; //
+           winners.clear(); // если появляется новый лидер, старого удаляем
+           winners.add(car.name); // добавляем в список текущего лидера
+       } else if (distance == maxDistance){
+           winners.add(car.name); // если кто-то справился также как и лидер, добавляем в список
        }
     }
     public void printWinner(){
-        System.out.println("Самая быстрая машина: " + winnerName); // метод, который выдает победителя
+        System.out.println("Самая быстрая машина: "); // метод, который выдает победителя
+
+        for (String winner : winners){
+            System.out.println(winner);
+        }
     }
 
    }
